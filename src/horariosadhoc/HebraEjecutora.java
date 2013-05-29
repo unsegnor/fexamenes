06b716aebbc0ad779ec9512a919a2e1cb5d6a833
unsegnor <lacuentadevictor@gmail.com>
2013-05-29 15:39:39 +0200

    Parelelizado

---
+++ src/horariosadhoc/HebraEjecutora.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horariosadhoc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Victor
+ */
+public class HebraEjecutora implements Runnable {
+    private final ArrayList<Asignatura> asignaturas;
+    private final int nhuecos;
+    private final HashMap<ParAsig, Integer> afectados;
+    private final int[] huecos;
+    private final ControladorDeHebras control;
+
+    HebraEjecutora(ControladorDeHebras control, ArrayList<Asignatura> asignaturas, int nhuecos, HashMap<ParAsig, Integer> afectados, int[] huecos) {
+        this.control = control;
+        this.asignaturas = asignaturas;
+        this.nhuecos = nhuecos;
+        this.afectados = afectados;
+        this.huecos = huecos;
+        
+    }
+
+    @Override
+    public void run() {
+
+            //Crear solución aleatoria
+            Sol solucion = Sol.Aleatoria(asignaturas, nhuecos);
+
+            //Mejorar
+            //System.out.println("--------------------------AL1-----------------------");
+            Sol sBL1 = Utiles.ALprimerMejor(solucion, afectados, huecos); //Primero mejoramos de forma aleatoria
+            //System.out.println("--------------------------AL2-----------------------");
+            //Sol sBL2 = Utiles.ALprimerMejor2(sBL1, dositemsets, huecos); //Después afinamos un poco
+            //System.out.println("--------------------------BL-----------------------");
+            Sol sBL = Utiles.BLprimerMejor(sBL1, afectados, huecos); //Después hacemos BL
+            Evaluacion eval = Sol.evaluarComp(sBL, afectados, huecos);
+
+
+            //System.out.println(mejor_eval);
+            //System.out.println(eval.colisiones + "\t"+ eval.puntos +"\t" + sBL);
+            control.heTerminado(eval, solucion);
+    }
+    
+}
